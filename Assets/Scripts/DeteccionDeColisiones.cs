7ff77fb009e16761ccf7e4039837cb4b24f47d1c
Maxistolki <49094305@est.ort.edu.ar>
2025-05-29 12:03:45 -0300

    collision caja collision caja collision
    y tambien ordena en enviroment y ndradasojdiojfeiwohfue

---
+++ Assets/Scripts/DeteccionDeColisiones.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColisiones : MonoBehaviour
+{
+    void OnCollisionEnter(Collision colission)
+    {
+        Debug.Log("Contacto");
+        Destroy(colission.gameObject);
+    }
+}
