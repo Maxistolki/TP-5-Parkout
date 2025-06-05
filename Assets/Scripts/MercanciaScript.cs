6f428030191b55058df5d51bd93212c6cad39479
Maxistolki <49094305@est.ort.edu.ar>
2025-06-05 12:18:24 -0300

    resta

--- Assets/Scripts/MercanciaScript.cs
+++ Assets/Scripts/MercanciaScript.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class MercanciaScript : MonoBehaviour
 {
+    public float cost;
+    public moneyManager MoneyManager;
+     void Start()
+    {
+        MoneyManager = FindObjectOfType<moneyManager>();
+    }
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Player")
         {
+            // actualizar el dinero p/c compra
+            MoneyManager.UpdateMoney(-cost);
             Destroy(gameObject);
         }
     }
